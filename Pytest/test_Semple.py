cbda8a8e13c012485de373042c8c39419128537f
Aleksander Karmazin (Home_NEW_PC) <kubartphil@gmail.com>
2020-01-01 21:21:55 +0300

    HTML Report Creating

--- Pytest/test_Semple.py
+++ Pytest/test_Semple.py
@@ -1,18 +1,46 @@
 from selenium import webdriver
+import pytest
 
+
+@pytest.fixture()
 def test_setup():
     global driver
-    driver = webdriver.Chrome('C:\Tools/chromedriver')  # Optional argument, if not specified will search path.
+    driver = webdriver.Chrome('C:/Tools/chromedriver')  # Optional argument, if not specified will search path.
     driver.implicitly_wait(10)
     driver.maximize_window()
+    yield
+    driver.quit()
+    print("Браузер тю тю ")
+    print("Браузер тю тю ")
 
-def test_loging():
+
+def test_loging(test_setup):
     driver.get("https://opensource-demo.orangehrmlive.com/")
     driver.find_element_by_id("txtUsername").send_keys("Admin")
     driver.find_element_by_id("txtPassword").send_keys("admin123")
     driver.find_element_by_id("btnLogin").click()
 
-def test_tearDown():
-    driver.quit()
-    print("Браузер тю тю ")
-    print("Браузер тю тю ")
+def test_loging1(test_setup):
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    driver.find_element_by_id("txtUsername").send_keys("Admin")
+    driver.find_element_by_id("txtPassword").send_keys("admin123")
+    driver.find_element_by_id("btnLogin").click()
+
+def test_loging2(test_setup):
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    driver.find_element_by_id("txtUsername").send_keys("Admin")
+    driver.find_element_by_id("txtPassword").send_keys("admin123")
+    driver.find_element_by_id("btnLogin").click()
+
+def test_loging3(test_setup):
+    driver.get("https://opensource-demo.orangehrmlive.com/")
+    driver.find_element_by_id("txtUsername").send_keys("Admin")
+    driver.find_element_by_id("txtPassword").send_keys("admin123")
+    driver.find_element_by_id("btnLogin").click()
+
+# def test_tearDown():
+#     driver.quit()
+#     print("Браузер тю тю ")
+#     print("Браузер тю тю ")
+
+
---
+++ Pytest/test_Semple2.py
@@ -0,0 +1,31 @@
+
+from selenium import webdriver
+import pytest
+
+class TestOrange():
+    @pytest.fixture()
+    def test_setup(self):
+        self.driver = webdriver.Chrome('C:/Tools/chromedriver')  # Optional argument, if not specified will search path.
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+        yield
+        self. driver.quit()
+        print("Браузер тю тю ")
+        print("Браузер тю тю ")
+
+
+    def test_loging(self,test_setup):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+
+    def test_loging1(self,test_setup):
+        self.driver.get("https://opensource-demo.orangehrmlive.com/")
+        self.driver.find_element_by_id("txtUsername").send_keys("Admin")
+        self.driver.find_element_by_id("txtPassword").send_keys("admin123")
+        self.driver.find_element_by_id("btnLogin").click()
+
+
+
+
